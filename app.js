82a9cb92d51f8ae2bd5fabac29268b231eb22bda
UwUBeebi <chris.erman@khk.ee>
2021-09-20 11:25:42 +0300

    Variables

---
+++ app.js
@@ -0,0 +1,35 @@
+// Muutujad
+// Var, let, const
+// Deklareerimine
+
+//let name = "Chris" ;
+// Kasutamine
+//console.log(name);
+//name = "Mairo" ;
+//console.log(name);
+
+// Lubatud märgid nime loomisel - tähed, numbrid, _, €
+// numbriga muutuja nimi ei tohi!
+
+//let € = "euro";
+//console.log();
+
+const name = "Chris";
+console.log(name);
+// Const ei saa muuta v22rtust
+//name = "Mairo";
+
+// Masiivid
+const Numbers = [1, 2,3 ,4 ,5];
+Numbers.push(6);
+console.log(Numbers);
+
+// Objektid
+const person = {
+	name: "Kadi" , 
+	age: 28
+};
+
+person.name = "Kati" ,
+person.age = 26
+console.log(person);
