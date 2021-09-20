35eff644fb04dcae07fc9614aa14d5d2d24a3eed
UwUBeebi <chris.erman@khk.ee>
2021-09-20 11:43:23 +0300

    Andmetuubid

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
-// Muutujad
-// Var, let, const
-// Deklareerimine
+// Andmetuubid
 
-//let name = "Chris" ;
-// Kasutamine
-//console.log(name);
-//name = "Mairo" ;
-//console.log(name);
+// Primitiivne
 
-// Lubatud märgid nime loomisel - tähed, numbrid, _, €
-// numbriga muutuja nimi ei tohi!
-
-//let € = "euro";
-//console.log();
-
+// String
 const name = "Chris";
-console.log(name);
-// Const ei saa muuta v22rtust
-//name = "Mairo";
+console.log(typeof name);
 
-// Masiivid
-const Numbers = [1, 2,3 ,4 ,5];
-Numbers.push(6);
-console.log(Numbers);
+// Number
+const number = 17;
+console.log(typeof number);
 
-// Objektid
+const persent = 50.3;
+console.log(typeof persent);
+
+// Boolean
+const tunnis = true;
+console.log(typeof tunnis);
+
+// Null
+const airplane = null;
+console.log(typeof airplane);
+
+// Undefined
+const test;
+console.log(typeof test);
+
+// Symbol
+const sym = Symbol();
+console.log(typeof sym);
+
+// Object
+const hobbies = ["games", "music"];
+console.log(typeof hobbies);
+
 const person = {
-	name: "Kadi" , 
-	age: 28
+	name: "Kati" ,
+	age: 22
 };
+console.log(typeof person);
 
-person.name = "Kati" ,
-person.age = 26
-console.log(person);
+const date = new Date();
+console.log(typeof date);
+console.log(date);
