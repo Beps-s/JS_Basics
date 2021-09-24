f8d32aa918bfc85a4ab1c355e1b07f05f72bd356
UwUBeebi <chris.erman@khk.ee>
2021-09-24 10:05:36 +0300

    Objektid

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-const num1 = [55, 26, 52, 74, 35, 16, 98];
-const num2 = new Array(54, 25, 53, 71, 37, 18, 96);
-const mixed = [22, 'Tere', true, null, undefined, new Date()];
+const person = {
+	firstName: 'Mati',
+	lastName: 'Karilaid',
+	age: 45,
+	email: 'mati.karilaid@tartuvangla.ee',
+	hobbies: ['korvpall', 'sport'],
+	address: {
+		city: 'Tartu',
+		country: 'Estonia'
+	},
+	getBirthYear: function() {
+		return 2021 - this.age;
+	}
+};
 
-let val;
+let val
 
-val = num1.length;
-val = Array.isArray(num1)
-val = num1[1];
-num1[1] = 100
-val = num1.indexOf(35);
-num1.push(250);
-num1.unshift(150);
-num1.pop();
-num1.shift(),
-//num1.splice(1, 3);
-//num1.reverse();
-val = num1.concat(num2);
-val =num1.sort();
-val = num1.sort(function(x, y){
-	return x - y;
-});
+val = person;
 
+val = person.firstName;
+val = person['lastName'];
+val = person.age;
+val = person.hobbies[0];
+val = person.address['city'];
+val = person.getBirthYear();
+
+const people = [
+	(name: 'Kati', age: 35),
+	(name: 'Mait', age: 23),
+	(name: 'Kait', age: 37)
+];
+
+val = people;
+
+for(let i = 0; i < people.length; i++){
+	console.log(people[i].name);
+}
 console.log(val);
-//console.log(num1);
