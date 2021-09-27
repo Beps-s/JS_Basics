87a90d24d3cbf1195b7e12081c137f1ca4f1fa8e
UwUBeebi <chris.erman@khk.ee>
2021-09-27 11:28:49 +0300

    tingimuslause/if

--- app.js
+++ app.js
@@ -1,28 +1,51 @@
-let val;
+const id = 100;
+//let testid;
 
-const today = new Date();
+// == v6rdub
+// != ei v6rdu
+// === v6rdub ja toimub tyybikontroll
+// !== ei v6rdu ja toimub tyybikontroll
 
-val = today.getMonth();
-val = today.getDate();
-val = today.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
-val = today.getTime();
+//if(typeof testid > 'undefined'){
+//	console.log(`ID on ${id} - suurem kui 100.`);
+//} else{
+//	console.log(`ID on ${id} - v2iksem v6i v6rdne 100.`);
+//}
 
-let birthday = new Date('11/9/2003')
-birthday = new Date('November 9 2003');
-let age = today - birthday;
+//console.log(typeof testid);
 
-birthday.setMonth(12);
-birthday.setDate(10);
-birthday.setFullYear(2003);
-birthday.setHours(11);
-birthday.setMinutes(31);
-birthday.setSeconds(51);
+/*const color = "green";
 
-console.log(birthday);
-console.log(today);
-console.log(age);
+if(color === 'red'){
+	console.log('Stop');
+} else if(color === 'yellow'){
+	console.log('Attention');
+} else if(color === 'green'){
+	console.log('Go');
+} else {
+	console.log('Color is not accepted');
+}
+*/
+const name = 'Kati';
+const age = 19;
+
+if(age > 0 && age <= 12){
+	console.log(`${name} on tatt`);
+} else if( age > 12 && age < 18){
+	console.log(`${name} on nooruk`);
+} else {
+	console.log(`${name} on t2iskasvanu`);
+}
+
+if(age < 18 || age > 65){
+	console.log(`${name} ei saa registreerida`);
+} else {
+	console.log(`${name} on registreeritud`);
+}
+
+//console.log(id === 100 ? 'Õige' : 'Vale');
+
+if(id === 100) 
+	console.log('ID Korras');
+else 
+	console.log('ID Ei ole korras');
