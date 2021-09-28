0edea5466b637435083d35d1e5d368ad3dad4655
UwUBeebi <chris.erman@khk.ee>
2021-09-28 09:50:35 +0300

    Funktsioonid

--- app.js
+++ app.js
@@ -1,51 +1,39 @@
-// for tsykkel
-for(let i = 0; i < 10; i++){
-	if(i == 5){
-		continue;
+// Funktsioon
+function greeting(firstName = 'Kadi', lastName = 'Tamm'){
 
+	/*if(typeof firstName === 'undefined'){
+		firstName = 'Kadi';
 	}
-	console.log(i)
+	if(typeof lastName === 'undefined'){
+		lastName = 'Tamm';
+	}*/
+	return 'Tere, ' + firstName + ' ' + lastName
+}
+console.log(greeting('Chris', 'Erman'));
+
+// Funktsioon avaldise kujul
+const square = function(number = 3) {
+	return number * number;
+}
+console.log(square())
+
+//
+(function(name){
+	console.log('Tere ' + name);
+})();
+
+const todo = {
+	add: function(){
+		console.log('add todo ...');
+	},
+	edit: function(id){
+		console.log(`edit todo nr ${id} ...`);
+	},
+	delete: function(){
+		console.log('delete todo ...');
+	}
 }
 
-// while tsykkel
-let i = 0;
-while(i < 10){
-	console.log(i)
-	i++;
-}
-
-//do while tsykkel
-let j = 0;
-do {
-	console.log(j)
-	j++
-} while(j < 10);
-
-// tsyklid ja massiivid
-const cars = ['Ford', 'Honda', 'Toyota'];
-
-for(let i = 0; i < cars.length; i++){
-	console.log(cars[i])
-}
-
-// for each tsykkel
-cars.forEach(function(element, index, array){
-	console.log(`cars: ${index} = ${element}`)
-	console.log(array)
-});
-
-cars.forEach((element, index) => {
-	console.log(`cars: ${index} = ${element}`)
-});
-
-
-const person = {
-	firstName: 'Mati',
-	lastName: 'Karilaid',
-	age: 37
-}
-
-// for in tsykkel
-for(let key in person){
-	console.log(`${key} = ${person[key]}`)
-}
+todo.add();
+todo.edit(5);
+todo.delete();
