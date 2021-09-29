b6a3f05ff92d5264cd2d2b9ae9110013060286b1
UwUBeebi <chris.erman@khk.ee>
2021-09-29 15:15:10 +0300

    Traveling DOM

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 let val;
 
-val = document.querySelector('li');
-const evenLi = document.querySelectorAll('li:nth-child(even)');
-const oddLi = document.querySelectorAll('li:nth-child(odd)');
+const list = document.querySelector('ul');
+const listItem = document.querySelector('li:first-child');
 
-evenLi.forEach(function(li){
-    li.style.background = '#ddd';
-});
+val = list;
+val = listItem;
 
-oddLi.forEach(function(li){
-    li.style.background = '#bbb';
-});
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[0].nodeType;
+
+val = list.children;
+val = list.children[1];
+list.children[1].textContent = 'XML';
+
+val = list.children[2].children;
+list.children[2].children[0].id = 'test-id';
+
+val = list.firstChild;
+val = list.firstElementChild;
+
+val = list.lastChild;
+val = list.lastElementChild;
+
+val = list.childElementCount;
+
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
+
+val = listItem.nextSibling;
+val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
+
+console.log(val);
