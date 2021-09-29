c508557857f50d6cecaa718864dcda498a643115
UwUBeebi <chris.erman@khk.ee>
2021-09-29 14:42:33 +0300

    DOM selectors for multiple elements

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
 let val;
 
-val = document.getElementById("task-title");
-val = document.getElementById("task-title").id;
+val = document.querySelector('li');
+const evenLi = document.querySelectorAll('li:nth-child(even)');
 
-const taskTitle = document.getElementById("task-title")
+evenLi.forEach(function(li){
+    li.style.background = '#ddd';
+});
 
-taskTitle.style.background = '#333';
-taskTitle.style.color = '#fff';
-taskTitle.style.padding = '5px';
-//taskTitle.style.display = 'none';
 
-taskTitle.textContent = 'My Tasks'
-taskTitle.innerText = 'My favorite tasks'
-taskTitle.innerHTML = '<span style="color: red;"">My Tasks</span>';
-
-val = document.querySelector('#task-title');
-
-document.querySelector('li').style.color = 'blue';
-document.querySelector('ul li').style.color = 'green';
-document.querySelector('li:last-child').style.color = 'red';
-
-console.log(val);
+console.log(evenLi);
