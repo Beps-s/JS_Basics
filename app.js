9e475e89635a8176f9f22e3146eb90faf6fc698e
UwUBeebi <chris.erman@khk.ee>
2021-10-08 12:35:52 +0300

    Add/Remove

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-const form=document.querySelector("form");
-const taskInput=document.getElementById("task");
-const heading=document.querySelector("h4");
-const li=document.createElement("li");
-const list=document.querySelector("ul");
+const form = document.querySelector('form');
+const taskInput = document.querySelector('#task');
+const taskList = document.querySelector('ul')
 
-li.className="collection-item";
-taskInput.value = "";
+form.addEventListener('submit', addToList)
+taskList.addEventListener('click', removeFromList)
 
-form.addEventListener("submit", addTask);
+function addToList(e) {
+    const li = document.createElement('li');
+    const list = document.querySelector('ul');
 
-function addTask(){
+    li.className = 'collection-item';
     li.appendChild(document.createTextNode(taskInput.value));
-    const link=document.createElement("a");
-    link.className="secondary-content red-text text-darken-2";
-    link.appendChild(document.createTextNode("X"));
-    link.setAttribute("href", "#");
+
+    const link = document.createElement('a');
+    link.className = 'secondary-content red-text text-darken-2'
+    link.appendChild(document.createTextNode('X'));
+    link.setAttribute('href', '#')
+
     li.appendChild(link);
     list.appendChild(li);
+
+    taskInput.value = '';
+
+    e.preventDefault();
+}
+
+function removeFromList(a) {
+    const target = event.target.parentElement;
+    target.remove();
 }
